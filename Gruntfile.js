1c4c11ff9f8926b98c98b3cf0fe537335a063e9f
Nick Webster <nick.webster@qlikview.com>
2016-05-17 18:20:55 +0100

    uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,9 +16,21 @@ module.exports = function(grunt) {
           livereload: true
         }
       }
+    },
+    uglify:{
+      options : {
+        beautify : false,
+        mangle   : true
+      },
+      build: {
+        files: {
+          'build/TubeMapViz.min.js': ['build/TubeMapViz.js']
+        }
+      }
     }
   });
   grunt.loadNpmTasks('grunt-includes');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default', ['includes','watch']);
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ['includes','uglify','watch']);
 };
